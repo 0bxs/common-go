fbc07f5a65c608c2e78d20740f8d2e8246308bbe
xc <1430158983@qq.com>
2025-11-28 12:37:32 +0800

    res

--- src/res/ok.go
+++ src/res/ok.go
@@ -4,3 +4,9 @@ type Ok[T any] struct {
 	Code int8 `json:"code"`
 	Data T    `json:"data"`
 }
+
+type Res[T any] struct {
+	Code int8   `json:"code"`
+	Msg  string `json:"msg,omitempty"`
+	Data T      `json:"data"`
+}
