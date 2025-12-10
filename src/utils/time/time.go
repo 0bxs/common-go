34627d88f40984ac32dad20493e4678ca9b4c0a5
xc <1430158983@qq.com>
2025-12-10 16:57:30 +0800

    time2

--- src/utils/time/time.go
+++ src/utils/time/time.go
@@ -1,9 +1,21 @@
 package time
 
-import "time"
+import (
+	"time"
+)
 
-func Now2Dawning() int64 {
+func Now2TodayEnd() int64 {
 	now := time.Now()
-	today := now.Truncate(24 * time.Hour)
-	return now.Sub(today).Milliseconds()
+	today := time.Date(now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, now.Location())
+	return today.Sub(now).Milliseconds()
+}
+
+func TodayEnd() int64 {
+	now := time.Now()
+	return time.Date(now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, now.Location()).UnixMilli() - 1
+}
+
+func TodayStart() int64 {
+	now := time.Now()
+	return time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location()).UnixMilli() - 1
 }
