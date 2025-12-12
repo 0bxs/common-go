9924144f25f611e66534cc8f90e4d072a1d36fb3
xc <1430158983@qq.com>
2025-12-12 16:52:49 +0800

    no router return

--- src/res/ok.go
+++ src/res/ok.go
@@ -1,5 +1,13 @@
 package res
 
+import (
+	"fmt"
+
+	"github.com/0bxs/common-go/src/status"
+	"github.com/0bxs/common-go/src/utils/rawJson"
+	"github.com/gofiber/fiber/v2"
+)
+
 type Ok[T any] struct {
 	Code int8 `json:"code"`
 	Data T    `json:"data"`
@@ -9,4 +17,16 @@ type Res[T any] struct {
 	Code int8   `json:"code"`
 	Msg  string `json:"msg,omitempty"`
 	Data T      `json:"data"`
+}
+
+var (
+	okJson = rawJson.RawJson(fmt.Sprintf(`{"code":%d}`, status.OK))
+)
+
+func Ok0(ctx *fiber.Ctx) error {
+	return ctx.JSON(okJson)
+}
+
+func Ok1[T any](ctx *fiber.Ctx, data T) error {
+	return ctx.JSON(Ok[T]{Code: status.OK, Data: data})
 }
