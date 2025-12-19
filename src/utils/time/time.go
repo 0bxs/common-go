c35fe6f404b398b5a0ef74f5a5002c5c41c3df9d
xc <1430158983@qq.com>
2025-12-19 15:53:54 +0800

    time

--- src/utils/time/time.go
+++ src/utils/time/time.go
@@ -15,7 +15,79 @@ func TodayEnd() int64 {
 	return time.Date(now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, now.Location()).UnixMilli() - 1
 }
 
+func TodayEnd1(now time.Time) int64 {
+	return time.Date(now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, now.Location()).UnixMilli() - 1
+}
+
 func TodayStart() int64 {
 	now := time.Now()
 	return time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location()).UnixMilli() - 1
+}
+
+func TodayStart1(now time.Time) int64 {
+	return time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location()).UnixMilli() - 1
+}
+
+func WeekStart() time.Time {
+	now := time.Now()
+	loc := now.Location()
+	weekday := int(now.Weekday())
+	if weekday == 0 {
+		weekday = 7
+	}
+	start := now.AddDate(0, 0, -(weekday - 1))
+	return time.Date(start.Year(), start.Month(), start.Day(), 0, 0, 0, 0, loc)
+}
+
+func WeekStart1(now time.Time) time.Time {
+	loc := now.Location()
+	weekday := int(now.Weekday())
+	if weekday == 0 {
+		weekday = 7
+	}
+	start := now.AddDate(0, 0, -(weekday - 1))
+	return time.Date(start.Year(), start.Month(), start.Day(), 0, 0, 0, 0, loc)
+}
+
+func WeekEnd() time.Time {
+	now := time.Now()
+	loc := now.Location()
+	weekday := int(now.Weekday())
+	if weekday == 0 {
+		weekday = 7
+	}
+	offset := 7 - weekday
+	end := now.AddDate(0, 0, offset)
+	return time.Date(end.Year(), end.Month(), end.Day(), 23, 59, 59, int(time.Second-time.Nanosecond), loc)
+}
+
+func WeekEnd1(now time.Time) time.Time {
+	loc := now.Location()
+	weekday := int(now.Weekday())
+	if weekday == 0 {
+		weekday = 7
+	}
+	offset := 7 - weekday
+	end := now.AddDate(0, 0, offset)
+	return time.Date(end.Year(), end.Month(), end.Day(), 23, 59, 59, int(time.Second-time.Nanosecond), loc)
+}
+
+func MontyStart() time.Time {
+	now := time.Now()
+	return time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location())
+}
+
+func MontyStart1(now time.Time) time.Time {
+	return time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location())
+}
+
+func MonthEnd() time.Time {
+	now := time.Now()
+	nextMonth := time.Date(now.Year(), now.Month()+1, 1, 0, 0, 0, 0, now.Location())
+	return nextMonth.Add(-time.Nanosecond)
+}
+
+func MonthEnd1(now time.Time) time.Time {
+	nextMonth := time.Date(now.Year(), now.Month()+1, 1, 0, 0, 0, 0, now.Location())
+	return nextMonth.Add(-time.Nanosecond)
 }
